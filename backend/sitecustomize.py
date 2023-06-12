a6561cde81662eca2ad9be596e7e14fdaa7a8dd7
Ana Cáceres <ana.caceres@leanmind.es>
2023-06-12 13:51:59 +0100

    Update sitecustomize.py

--- backend/sitecustomize.py
+++ backend/sitecustomize.py
@@ -1,10 +1,5 @@
-# install the apport exception handler if available
-import sys
-from sage.all import *
-
-sys.modules[__name__].__dict__.update(ZZ=ZZ)
-
 try:
+    from sage.all import *
     import apport_python_hook
 except ImportError:
     pass
